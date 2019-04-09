a53a73155c3c394a327b52d6b053a8c99bc28026
Marco Chagas Costa <marco.chagas.costa@hotmail.com>
2019-04-08 23:33:47 -0300

    marco

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { EventoService } from '../_services/evento.service';
 import { Evento } from '../_models/Evento';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { defineLocale, BsLocaleService, ptBrLocale } from 'ngx-bootstrap';
+defineLocale('pt-br', ptBrLocale);
 
 @Component({
   selector: 'app-eventos',
@@ -12,17 +15,26 @@ export class EventosComponent implements OnInit {
 
   eventosFiltrados: Evento[];
   eventos: Evento[];
+
+  evento: Evento;
+  modoSalvar = 'post';
+
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
-  modalRef: BsModalRef;
+  registerForm: FormGroup;
 
   filtrarLista = '';
+  bodyDeletarEvento: string;
 
   constructor(
       private eventoService: EventoService
     , private modalService: BsModalService
-  ) { }
+    , private fb: FormBuilder
+    , private localeService: BsLocaleService
+  ) {
+    this.localeService.use('pt-br');
+  }
 
   get filtroLista(): string {
     return this.filtrarLista;
@@ -32,11 +44,42 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template);
+  editarEvento(evento: Evento, template: any) {
+    this.modoSalvar = 'put';
+    this.openModal(template);
+    this.evento = evento;
+    this.registerForm.patchValue(evento);
+  }
+
+  novoEvento(template: any) {
+    this.modoSalvar = 'post';
+    this.openModal(template);
+  }
+
+  excluirEvento(evento: Evento, template: any) {
+    this.openModal(template);
+    this.evento = evento;
+    this.bodyDeletarEvento = `Tem certeza que deseja excluir o Evento: ${evento.tema}, Código: ${evento.id}`;
+  }
+
+  confirmeDelete(template: any) {
+    this.eventoService.deleteEvento(this.evento.id).subscribe(
+      () => {
+          template.hide();
+          this.getEventos();
+        }, error => {
+          console.log(error);
+        }
+    );
+  }
+
+  openModal(template: any) {
+    this.registerForm.reset();
+    template.show();
   }
 
   ngOnInit() {
+    this.validation();
     this.getEventos();
   }
 
@@ -51,12 +94,51 @@ export class EventosComponent implements OnInit {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
+  validation() {
+    this.registerForm = this.fb.group({
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoas: ['',
+      [Validators.required, Validators.max(120000)]],
+      imagemURL: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  salvarAlteracao(template: any) {
+    if (this.registerForm.valid) {
+      if (this.modoSalvar === 'post') {
+        this.evento = Object.assign({}, this.registerForm.value);
+        this.eventoService.postEvento(this.evento).subscribe(
+            (novoEvento: Evento) => {
+              template.hide();
+              this.getEventos();
+            }, error => {
+              console.log(error);
+            }
+        );
+      } else {
+        this.evento = Object.assign({id : this.evento.id}, this.registerForm.value);
+        this.eventoService.putEvento(this.evento).subscribe(
+            () => {
+              template.hide();
+              this.getEventos();
+            }, error => {
+              console.log(error);
+            }
+        );
+      }
+    }
+  }
+
   getEventos() {
     this.eventoService.getAllEvento().subscribe(
-    (_eventos: Evento[]) => {
-      this.eventos = _eventos;
+    (eEventos: Evento[]) => {
+      this.eventos = eEventos;
       this.eventosFiltrados = this.eventos;
-      console.log(_eventos);
+      console.log(eEventos);
     }, error => {
       console.log(error);
     });
